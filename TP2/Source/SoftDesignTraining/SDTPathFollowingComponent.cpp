070f5b2b544e11080c670e327f83118aaf9d2b34
PhilLec <unconfigured@null.spigotmc.org>
2023-03-12 16:27:59 -0400

    logique de saut

--- TP2/Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
+++ TP2/Source/SoftDesignTraining/SDTPathFollowingComponent.cpp
@@ -10,27 +10,65 @@
 
 USDTPathFollowingComponent::USDTPathFollowingComponent(const FObjectInitializer& ObjectInitializer)
 {
-
+    Initialize();
 }
 
 void USDTPathFollowingComponent::FollowPathSegment(float DeltaTime)
 {
-    Super::FollowPathSegment(DeltaTime);
     const TArray<FNavPathPoint>& points = Path->GetPathPoints();
+
     const FNavPathPoint& segmentStart = points[MoveSegmentStartIndex];
-    // UE_LOG(LogTemp, Warning, TEXT("FollowPathSegment"));
+    FNavPathPoint segmentEnd;
+    if (points.Num() > MoveSegmentEndIndex) {
+        segmentEnd = points[MoveSegmentEndIndex];
+    }
+
+    FVector start = segmentStart.Location;
+    FVector end = segmentEnd.Location;
 
     ASDTAIController* controller = dynamic_cast<ASDTAIController*>(GetOwner());
     if (SDTUtils::HasJumpFlag(segmentStart))
     {
         //update jump
         controller->AtJumpSegment = true;
-        GEngine->AddOnScreenDebugMessage(-1, 0, FColor::Blue, FString::Printf(TEXT("===========================")));
+
+        AActor* pawn = controller->GetPawn();
+        UCurveFloat* jumpCurve = controller->JumpCurve;
+        float jumpSpeed = controller->JumpSpeed;
+        float jumpApexHeight = controller->JumpApexHeight;
+        FVector vecToEnd = FVector(end - start);
+        vecToEnd.Z = 0.f;
+
+        float jumpTime = lastJumpTime + DeltaTime*jumpSpeed;
+        lastJumpTime = jumpTime;
+
+        FVector newPos = start + vecToEnd * jumpTime;
+        float curveValue = jumpCurve->GetFloatValue(jumpTime);
+        newPos.Z = jumpApexHeight * curveValue + 216.f;
+
+        pawn->SetActorLocation(newPos, true);
+        if (jumpTime >= 1) {
+            pawn->SetActorLocation(end);
+        }
+
+        FVector pawnPos = pawn->GetActorLocation();
+
+        if (pawnPos.Z >= 260) {
+            controller->InAir = true;
+        }
+
+        if (pawnPos.Z <= 265) {
+            controller->Landing = true;
+        }
     }
     else
     {
         //update navigation along path
-        
+        Super::FollowPathSegment(DeltaTime);
+        controller->AtJumpSegment = false;
+        controller->InAir = false;
+        controller->Landing = false;
+        lastJumpTime = 0;
     }
 }
 
